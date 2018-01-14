ca2609c3560a48c60c1105cdea9885a505c42ca6
David L. Jessup <djessup@usnews.com>
2018-01-14 16:55:38 -0500

    Get all completed items, not just some.
    Because we were using the Todoist API's get_completed method, we
    were limited to retrieving only the last 30 completed tasks.  We
    worked around this by using the activity log.

--- export-done.py
+++ export-done.py
@@ -14,6 +14,45 @@ def get_api_token():
     return config['main']['api-key']
 
 
+def get_completed_items(api, project_id, month):
+    # The naïve approach for the following code would be to simply return
+    # api.items.get_completed(project_id).
+    # However, the Todoist API only returns the last 30 completed records and doesn't provide a
+    # documented direct mechanism to page for more records.
+    # We work around this by using the activity log to find the IDs of items that have been
+    # completed at least once.
+    # We pass in the month so that we can limit the amount of activity logs that we have to
+    # page through.
+    # so
+
+    activity = []
+    activity_buffer = None
+    offset = 0
+    limit = 100
+    while activity_buffer is None or len(activity_buffer) > 0:
+        activity_buffer = api.activity.get(
+            object_type='item', event_type='completed', parent_project_id=project_id,
+            limit=limit, since=(month + '-01T00:00'), offset=offset)
+        activity += activity_buffer
+        offset += limit
+
+    # Because we're using the activity logs, we may pick up duplicate item IDs (if an item is
+    # recurring, or if an item was completed in error, uncompleted, and then recompleted
+    # later).
+    completed_item_ids = set([event['object_id'] for event in activity])
+    completed_items = []
+    for item_id in completed_item_ids:
+        item = api.items.get_by_id(item_id)
+        if type(item) == dict:
+            item = item['item']
+
+        # Because we're using the activity logs, we may pick up an item that is still active,
+        # if the item is recurring or if the item was uncompleted after being completed.
+        if item['in_history'] == 1:
+            completed_items.append(item)
+    return completed_items
+
+
 def get_project_id(api, project_name):
     project_id = None
     for project in api.state['projects']:
@@ -40,7 +79,9 @@ def export_done(project_name, month, showtime):
     print(heading_border)
     print(heading)
     print(heading_border)
-    completed_items = api.items.get_completed(project_id)
+
+    completed_items = get_completed_items(api, project_id, month)
+
     log_dates = {}
     for item in completed_items:
         item_datetime = dateutil.parser.parse(item['due_date_utc']).astimezone()
